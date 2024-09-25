820bca887c1b5ceefba26e5df3099ffa4751f41c
HARSH-THAKUR-07 <harshthakur0971@gmail.com>
2024-09-25 21:35:40 +0530

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request
-from transformers import pipeline
 
 app = Flask(__name__)
-summarizer = pipeline("summarization")
+
+def simple_summarizer(text, num_sentences=2):
+    sentences = text.split('. ')
+    summary = '. '.join(sentences[:num_sentences]) + ('.' if len(sentences) > num_sentences else '')
+    return summary
 
 @app.route('/')
 def home():
@@ -11,8 +14,8 @@ def home():
 @app.route('/summarize', methods=['POST'])
 def summarize():
     text = request.form['text']
-    summary = summarizer(text, max_length=130, min_length=30, do_sample=False)
-    return render_template('index.html', summary=summary[0]['summary_text'])
+    summary = simple_summarizer(text)
+    return render_template('index.html', summary=summary)
 
 if __name__ == '__main__':
     app.run(debug=True)
